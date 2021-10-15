89fd3ff0c5ccbc62b17c45e84788dcd919f594ae
derganc <derganc@iskratel.si>
2021-10-15 16:25:37 +0200

    get step.2 handle slashes in callSign

--- api/get.go
+++ api/get.go
@@ -4,11 +4,34 @@ import (
 	"errors"
 	"fmt"
 	"github.com.s51ds/ctydb/cty"
+	"strings"
 )
 
 var records map[string]cty.Dat
 
 func Get(callSign string) (countryData cty.Dat, err error) {
+	callSign = strings.TrimSpace(callSign)
+	callSign = strings.ToUpper(callSign)
+
+	if strings.Contains(callSign, "/") {
+		// handle /P /MM etc
+		callSign = strings.TrimSuffix(callSign, "/P")
+		callSign = strings.TrimSuffix(callSign, "/MM")
+		callSign = strings.TrimSuffix(callSign, "/QRP")
+		callSign = strings.TrimSuffix(callSign, "/M")
+		callSign = strings.TrimSuffix(callSign, "/AM")
+		callSign = strings.TrimSuffix(callSign, "/A")
+		callSign = strings.TrimSuffix(callSign, "/LH")
+
+		// handling such cases like AC2AI/KH2 --> KH2/AC2AI
+		if strings.Contains(callSign, "/") {
+			ss := strings.Split(callSign, "/")
+			if len(ss) == 2 && len(ss[0]) > len(ss[1]) {
+				callSign = ss[1] + "/" + ss[0]
+			}
+		}
+	}
+
 	for i := len(callSign); i >= 0; i-- {
 		s := callSign[:i]
 		if v, has := records[s]; has {
--- api/get_test.go
+++ api/get_test.go
@@ -18,6 +18,24 @@ func TestGet(t *testing.T) {
 	}{
 		{
 			name: "s59abc",
+			args: args{"s59abc"},
+			wantCountryData: cty.Dat{
+				CountryName:   "Slovenia",
+				PrimaryPrefix: "S5",
+				AliasPrefix:   "S5",
+				Continent:     "EU",
+				CqZone:        "15",
+				ItuZone:       "28",
+				LatLon: cty.LatLonDeg{
+					Lat: 46,
+					Lon: 14,
+				},
+				TimeOffset: "-1.0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "S59ABC",
 			args: args{"S59ABC"},
 			wantCountryData: cty.Dat{
 				CountryName:   "Slovenia",
@@ -31,6 +49,135 @@ func TestGet(t *testing.T) {
 					Lon: 14,
 				},
 				TimeOffset: "-1.0",
+			},
+			wantErr: false,
+		},
+
+		{
+			name: "S51DS/P",
+			args: args{"S51DS/P"},
+			wantCountryData: cty.Dat{
+				CountryName:   "Slovenia",
+				PrimaryPrefix: "S5",
+				AliasPrefix:   "S5",
+				Continent:     "EU",
+				CqZone:        "15",
+				ItuZone:       "28",
+				LatLon: cty.LatLonDeg{
+					Lat: 46,
+					Lon: 14,
+				},
+				TimeOffset: "-1.0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "9A/S51DS/P",
+			args: args{"9A/S51DS/P"},
+			wantCountryData: cty.Dat{
+				CountryName:   "Croatia",
+				PrimaryPrefix: "9A",
+				AliasPrefix:   "9A",
+				Continent:     "EU",
+				CqZone:        "15",
+				ItuZone:       "28",
+				LatLon: cty.LatLonDeg{
+					Lat: 45.18,
+					Lon: 15.3,
+				},
+				TimeOffset: "-1.0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "9A/S51DS",
+			args: args{"9A/S51DS"},
+			wantCountryData: cty.Dat{
+				CountryName:   "Croatia",
+				PrimaryPrefix: "9A",
+				AliasPrefix:   "9A",
+				Continent:     "EU",
+				CqZone:        "15",
+				ItuZone:       "28",
+				LatLon: cty.LatLonDeg{
+					Lat: 45.18,
+					Lon: 15.3,
+				},
+				TimeOffset: "-1.0",
+			},
+			wantErr: false,
+		},
+
+		{
+			name: "S51DS/MM",
+			args: args{"S51DS/MM"},
+			wantCountryData: cty.Dat{
+				CountryName:   "Slovenia",
+				PrimaryPrefix: "S5",
+				AliasPrefix:   "S5",
+				Continent:     "EU",
+				CqZone:        "15",
+				ItuZone:       "28",
+				LatLon: cty.LatLonDeg{
+					Lat: 46,
+					Lon: 14,
+				},
+				TimeOffset: "-1.0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "S51DS/M",
+			args: args{"S51DS/M"},
+			wantCountryData: cty.Dat{
+				CountryName:   "Slovenia",
+				PrimaryPrefix: "S5",
+				AliasPrefix:   "S5",
+				Continent:     "EU",
+				CqZone:        "15",
+				ItuZone:       "28",
+				LatLon: cty.LatLonDeg{
+					Lat: 46,
+					Lon: 14,
+				},
+				TimeOffset: "-1.0",
+			},
+			wantErr: false,
+		},
+
+		{
+			name: "AC2AI/KH2",
+			args: args{"AC2AI/KH2"},
+			wantCountryData: cty.Dat{
+				CountryName:   "Guam",
+				PrimaryPrefix: "KH2",
+				AliasPrefix:   "KH2",
+				Continent:     "OC",
+				CqZone:        "27",
+				ItuZone:       "64",
+				LatLon: cty.LatLonDeg{
+					Lat: 13.37,
+					Lon: 144.7,
+				},
+				TimeOffset: "-10.0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "KH2/AC2AI",
+			args: args{"KH2/AC2AI"},
+			wantCountryData: cty.Dat{
+				CountryName:   "Guam",
+				PrimaryPrefix: "KH2",
+				AliasPrefix:   "KH2",
+				Continent:     "OC",
+				CqZone:        "27",
+				ItuZone:       "64",
+				LatLon: cty.LatLonDeg{
+					Lat: 13.37,
+					Lon: 144.7,
+				},
+				TimeOffset: "-10.0",
 			},
 			wantErr: false,
 		},
